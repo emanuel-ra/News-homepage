5b61d00a4d356f17856eb07392f8a5a4ad26ae37
Tomas Emanuel Ramirez Abarca <emanuelrr55@gmail.com>
2023-12-05 02:14:02 -0600

    Add: Footer

--- App/src/App.jsx
+++ App/src/App.jsx
@@ -1,11 +1,10 @@
-import NavBar from "./components/NavBar"
-import Post from "./components/Post"
-import News from "./components/News"
-import Card from "./components/Card"
-import { RetrpPCs, Laptops, Gaming } from "./assets/images"
+import NavBar from "./components/NavBar";
+import Post from "./components/Post";
+import News from "./components/News";
+import Card from "./components/Card";
+import { RetrpPCs, Laptops, Gaming } from "./assets/images";
 
 function App() {
-
   return (
     <>
       <div className="relative max-w-[1440px] min-w-[350px] lg:w-[1440px] flex flex-wrap justify-center p-2 gap-y-6 lg:gap-6">
@@ -18,25 +17,42 @@ function App() {
         </aside>
 
         <article className="w-full flex flex-wrap justify-between gap-4 animate-fade animate-once animate-delay-100 animate-ease-in-out delay-150">
-          <Card 
-            image={RetrpPCs} 
-            order={`01`} 
-            title={`Reviving Retro PCs`} 
-            paragraph={`What happens when old PCs are given modern upgrades?`} />
-          <Card 
-            image={Laptops} 
-            order={`02`} 
-            title={`Top 10 Laptops of 2022`} 
-            paragraph={`Our best picks for various needs and budgets.`} />
-          <Card 
-            image={Gaming} 
-            order={`03`} 
-            title={`The Growth of Gaming`} 
-            paragraph={`How the pandemic has sparked fresh opportunities.`} />
+          <Card
+            image={RetrpPCs}
+            order={`01`}
+            title={`Reviving Retro PCs`}
+            paragraph={`What happens when old PCs are given modern upgrades?`}
+          />
+          <Card
+            image={Laptops}
+            order={`02`}
+            title={`Top 10 Laptops of 2022`}
+            paragraph={`Our best picks for various needs and budgets.`}
+          />
+          <Card
+            image={Gaming}
+            order={`03`}
+            title={`The Growth of Gaming`}
+            paragraph={`How the pandemic has sparked fresh opportunities.`}
+          />
         </article>
-      </div>     
+
+        <footer className="animate-fade animate-once animate-delay-100 animate-ease-in-out delay-200">
+          Challenge by {" "}
+          <a
+            href="https://www.frontendmentor.io?ref=challenge"
+            target="_blank"
+            rel="noreferrer"
+            className="font-semibold text-very-dark-blue hover:text-soft-red"
+          >
+            Frontend Mentor
+          </a>
+
+          . Coded by <a className="font-semibold text-very-dark-blue hover:text-soft-red" href="https://www.linkedin.com/in/emanuelramirezabarca/" target="_blank" rel="noreferrer">Tomas Emanuel Ramirez Abarca</a>.
+        </footer>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
